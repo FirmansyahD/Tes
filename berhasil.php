c9ae22db324a3ccea702b5cb9e02ef8d46fcfea7
FirmansyahD <dewantaraf855@gmail.com>
2019-01-12 08:33:43 +0700

    ya

--- berhasil.php
+++ berhasil.php
@@ -9,34 +9,37 @@
 </head>
 <body>
     <div class="container">
-    	<form action="<?php $_SERVER['PHP_SELF'];?>" method="post" class="form-group">
-	    <table>
-	    <tr>
-	    <td>NIS</td>
-	    <input type="hidden"name="id_siswa">
-	    <td><input class="form-control"type="text"name="nis" size=40aria-describedby="nisHelp"></td>    
-	    </tr>
-	    <tr>
-	    <td>Nama</td>
-	    <td><input class="form-control"type="text"name="nama"></td>
-	    </tr>
-	    <tr>
-	    <td>Alamat</td>
-	    <td><input type="text"name="alamat"class="form-control" id="alamat" aria-describedby="alamatHelp" ></td>
-	    </tr>
-	    <tr>
-	    <td>Password</td>
-	    <td><input class="form-control"aria-describedby="passwordHelp" type="password"name="password"></td>
-	    </tr>
-	    
-	    <tr>
-	    <td></td>
-	    <td><input type="submit" value="Simpan"class="btn btn-primary"></td>
-	    </tr>
-	    
-	    </table>
-	    </form>
-    </div>
+    	<div class="row">
+    		<div class="col">
+    			<br>
+		    	<form action="<?php $_SERVER['PHP_SELF'];?>" method="post" class="form-group">
+			    <table>
+			    <tr>
+			    <td>NIS</td>
+			    <input type="hidden"name="id_siswa">
+			    <td><input class="form-control"type="text"name="nis" size=40aria-describedby="nisHelp"></td>    
+			    </tr>
+			    <tr>
+			    <td>Nama</td>
+			    <td><input class="form-control"type="text"name="nama"></td>
+			    </tr>
+			    <tr>
+			    <td>Alamat</td>
+			    <td><input type="text"name="alamat"class="form-control" id="alamat" aria-describedby="alamatHelp" ></td>
+			    </tr>
+			    <tr>
+			    <td>Password</td>
+			    <td><input class="form-control"aria-describedby="passwordHelp" type="password"name="password"></td>
+			    </tr>
+			    
+			    <tr>
+			    <td></td>
+			    <td><input type="submit" value="Simpan"class="btn btn-primary"></td>
+			    </tr>
+			    
+			    </table>
+			    </form>
+    		</div>
     <!-- <br>
     <div class="container">
     	<form method="post" class="form-group" action="<?php $_SERVER['PHP_SELF'];?>">
@@ -50,42 +53,45 @@
 			<input type="submit" name="SUBMIT" value="Simpan" class="btn btn-primary"><br>
     	</form>
     </div> -->
+    <div class="col">
+    	<?php
+		require_once "./koneksi.php";
+		if(isset($_POST['nis']) && isset($_POST['nama'])){
+			$id_siswa=$_POST['id_siswa'];
+		    $nis=$_POST['nis'];
+		    $nama=$_POST['nama'];
+		    $alamat=$_POST['alamat'];
+		    $password=$_POST['password'];
+		    $sql="INSERT INTO siswa VALUES ('".$id_siswa."','".$nis."','".$nama."','".$alamat."','".$password."')";
+		    $result=mysqli_query($conn,$sql);
+		    if($result){
+		        echo"<script>alert('Berhasil Menambahkan Data')</script>";
+		    }
+		        else{
+		            echo"<script>alert('Gagal Menambahkan Data')</script>";
+		            echo mysqli_error($conn);
+		        
+		    }
+		}
+		require_once "./record.php";
+		?>
+
+		<?php
+		if(isset($_POST['logout'])) {
+		  //Unset cookies and other things you want to
+		  session_destroy();
+		  header('Location: index.php'); //Dont forget to redirect
+		  exit;
+		}
+		?>
+		<form method="POST" style="float: right;">
+		  <input type="submit" name="logout" value="logout" class="btn btn-primary">
+		</form>
+    </div>
+    </div>
+	</div>
 </body>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-
-<?php
-require_once "./koneksi.php";
-if(isset($_POST['nis']) && isset($_POST['nama'])){
-	$id_siswa=$_POST['id_siswa'];
-    $nis=$_POST['nis'];
-    $nama=$_POST['nama'];
-    $alamat=$_POST['alamat'];
-    $password=$_POST['password'];
-    $sql="INSERT INTO siswa VALUES ('".$id_siswa."','".$nis."','".$nama."','".$alamat."','".$password."')";
-    $result=mysqli_query($conn,$sql);
-    if($result){
-        echo"<script>alert('Berhasil Menambahkan Data')</script>";
-    }
-        else{
-            echo"<script>alert('Gagal Menambahkan Data')</script>";
-            echo mysqli_error($conn);
-        
-    }
-}
-echo'<hr />';
-require_once "./record.php";
-?>
-
-<?php
-if(isset($_POST['logout'])) {
-  //Unset cookies and other things you want to
-  session_destroy();
-  header('Location: index.php'); //Dont forget to redirect
-  exit;
-}
-?>
-<form method="POST">
-  <input type="submit" name="logout" value="logout"class="btn btn-primary">
-</form>
+</html>
--- record.php
+++ record.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<br>
 <?php
 require_once "./koneksi.php";
 $sql='SELECT * FROM siswa';
@@ -5,22 +7,26 @@ $result=mysqli_query($conn,$sql);
 if($result){
     if(mysqli_num_rows($result)){
         ?>
-        <table>
-        <tr>
-        <td>#</td>
-        <td width=100>NIS</td>
-        <td witdth=150>Nama</td>
-        <td>alamat</td>
-        </tr>       
+        <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">NIS</th>
+            <th scope="col">Nama</th>
+            <th scope="col">alamat</th>
+            </tr>  
+        </thead>     
 <?php
 $i=1;
 while($row=mysqli_fetch_row($result)){
 ?>    
-<tr>
-<td><?php echo $i; ?></td>
-<td><?php echo $row[1];?></td>
-<td><?php echo $row[2];?></td>
-<td><?php echo $row[4];?></td></tr>
+<tbody>
+    <tr>
+    <td><?php echo $i; ?></td>
+    <td><?php echo $row[1];?></td>
+    <td><?php echo $row[2];?></td>
+    <td><?php echo $row[4];?></td></tr>
+</tbody>
 <?php $i++;
 } ?>
 </table>
@@ -32,3 +38,6 @@ else{
 mysqli_close($conn);
 }
 ?>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
